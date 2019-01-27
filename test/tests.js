fa6b658df8004a6daf4d9783b7e88bb1090a3981
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-01-27 15:10:06 -0500

    Added test for single dominero container scenario.

--- test/tests.js
+++ test/tests.js
@@ -3,6 +3,34 @@ const jsdom = require('jsdom');
 const dominero = require('./../.node-index.js').default;
 const { JSDOM } = jsdom;
 
+describe('one dominero container', function () {
+  const dom = new JSDOM(`
+    <div class="container1">
+      <h1 data-dominero="title">Default Title 1</h1>
+      <p data-dominero="description">Default Description 1</p>
+    </div>
+  `, {
+    url: 'http://localhost'
+  });
+
+  const container1 = dominero(dom.window.document.querySelector('.container1'), {
+    title: 'Hello, World!',
+    description: 'One way data binding is all a go!'
+  });
+
+  container1.title = 'I was updated';
+
+  it('initializes a property', function () {
+    expect(dom.window.document.querySelector('.container1 [data-dominero="description"]').textContent)
+    .eql('One way data binding is all a go!')
+  });
+
+  it('updates a property', function () {
+    expect(dom.window.document.querySelector('.container1 [data-dominero="title"]').textContent)
+    .eql('I was updated')
+  });
+});
+
 describe('two dominero containers side by side', function () {
   const dom = new JSDOM(`
     <div class="container1">
@@ -29,7 +57,7 @@ describe('two dominero containers side by side', function () {
 
   container2.title = 'I was updated';
 
-  it('initialized a property', function () {
+  it('initializes a property', function () {
     expect(dom.window.document.querySelector('.container1 [data-dominero="title"]').textContent)
     .eql('Hello, World!')
   });
